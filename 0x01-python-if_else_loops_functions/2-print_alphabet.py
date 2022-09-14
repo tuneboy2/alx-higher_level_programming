898488a8158fc6fde8f7fef3573f48ae7149ca34
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-09-14 07:38:54 -0700

    Task 3

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
+# The ord() function returns the number representing the unicode code of a specified character.
+# The chr() function returns the character that represents the specified unicode.
+
 for c in range(ord('a'), ord('z') + 1):
     print("{}".format(chr(c)), end='')
