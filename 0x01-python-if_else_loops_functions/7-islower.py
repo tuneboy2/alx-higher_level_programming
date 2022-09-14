53dd1d47105c13efd905133a0b92a894474fd034
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-09-14 13:36:21 -0700

    Task 7

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def islower(c):
-    if c >= chr(ord('a')) and c <= chr(ord('z')):
+    c = ord(c)
+    if c >= ord('a') and c <= ord('z'):
         return True
     else:
         return False
