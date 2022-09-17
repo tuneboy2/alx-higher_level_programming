385c8f76f7d76f768d1cddf235648a30356b07fd
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-09-17 16:53:56 -0700

    Task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    '''Prints a Matrix of Integers'''
+
+    if matrix == [[]]:
+        print()
+
+    for row in matrix:
+        for i in range(len(row)):
+            if i < len(row) - 1:
+                print("{:d}".format(row[i]), end=' ')
+            else:
+                print("{:d}".format(row[i]))
