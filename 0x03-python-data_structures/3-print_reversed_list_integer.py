2cfbab8a9e99608cf3db1d3593ee5fde823dd0ad
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-09-17 13:17:24 -0700

    Task 3

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def print_reversed_list_integer(my_list=[]):
+    '''Prints all integers of a list in reverse mode one integer per line'''
+
+    j = len(my_list)
+    list_cpy = my_list[:]
+
+    for i in range(j):
+        my_list[i] = list_cpy[j - 1]
+        j -= 1
+        print("{:d}".format(my_list[i]))
