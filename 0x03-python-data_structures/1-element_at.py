7e3ac6326e3f536e9be1bb9ff9f1e51269c290f2
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-09-18 07:33:28 -0700

    Task 9

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -6,7 +6,7 @@ def element_at(my_list, idx):
         If idx is negative:
             return none
 
-        idx is out of range (> of number of element in my_list):
+        if idx is out of range (> of number of element in my_list):
             return none
     '''
     if idx < 0 or idx >= len(my_list):
---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    '''Finds the biggest integer of a list.
+
+        If the list is empty:
+            return None
+    '''
+
+    maxima = 0
+    for i in range(len(my_list)):
+        if my_list[i] > maxima:
+            maxima = my_list[i]
+
+    return maxima
