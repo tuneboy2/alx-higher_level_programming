03bf5c2cf95b1fea2f9c26bcf51e4e6b066c906d
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-09-18 14:49:39 -0700

    Task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,5 +2,5 @@
 
 def search_replace(my_list, search, replace):
     '''Replaces all occurrences of an element by another in a new list'''
-    new = list(map(lambda x : replace if search == x else x, my_list))
+    new = list(map(lambda x: replace if search == x else x, my_list))
     return new
