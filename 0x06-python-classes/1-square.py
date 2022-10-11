7f73693fe7ee932d63985de3474b90347f815ea6
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-10-11 07:22:16 -0700

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Defines a Class Square"""
+
+
+class Square:
+    """ Represents a square """
+    def __init__(self, value):
+        """ Initializes self to the parameters """
+        self.__size = value
