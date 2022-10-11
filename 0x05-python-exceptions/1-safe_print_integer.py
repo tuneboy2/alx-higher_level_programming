f9f3027b0530aae37716895bf9e832f41fc7cafc
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-10-10 20:38:22 -0700

    Task 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -9,6 +9,5 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except:
-        None
-    return False
+    except(TypeError, ValueError):
+        return False
