b7832ffb65cd99ed44c84300bab3f5c3de74da52
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-10-11 19:36:19 -0700

    Task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" Defines a class Square """
+
+
+class Square:
+    """ Represents a square """
+    def __init__(self, value=0):
+        """ Initializes parameters to self
+
+        Args:
+            value: the size of the square
+            """
+        self.size = value
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        try:
+            assert type(value) == int
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+        except AssertionError:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """ Returns the area of a square """
+        return self.size ** 2
+
+    def my_print(self):
+        if self.size == 0:
+            print()
+            return
+        for x in range(self.size):
+            for y in range(self.size):
+                print("#", end="")
+            print()
