a12276052576f9fe2cfdd651993b6ee999678ff4
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-10-15 03:23:43 -0700

    Add ignore for exception details

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -12,6 +12,7 @@ def say_my_name(first_name, last_name=""):
     Raises:
     TypeError: if parameter passed is not of type stting
     """
+
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
