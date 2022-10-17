db19363a0e69211345812f098b56fb7b677c365a
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-10-17 11:58:32 -0700

    Task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,8 +5,8 @@
 class Rectangle:
     """ defines the representation for a rectangle """
     def __init__(self, width=0, height=0):
-        self.height = height
-        self.width = width
+        self.__height = height
+        self.__width = width
 
     @property
     def width(self):
--- 0x08-python-more_classes/tests/1-main.py
+++ 0x08-python-more_classes/tests/1-main.py
@@ -5,7 +5,7 @@ my_rectangle = Rectangle(2,4)
 print(my_rectangle.__dict__)
 
 my_rectangle.width = 10
-my_rectangle.height = 3
+my_rectangle.height = "f"
 print(my_rectangle.__dict__)
 
 print("{}".format(my_rectangle.height))
