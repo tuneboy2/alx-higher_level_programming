61885af0fbb8c0a7ae37ab051316a4f5b4014970
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-10-24 16:51:25 -0700

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Define a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """An empty instance of class BaseGeometry"""
+    def area(self):
+        """ An undefinrd area method of class BaseGeometry"""
+        raise Exception("area() is not implemented")
