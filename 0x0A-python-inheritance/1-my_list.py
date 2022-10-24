4876df07ce1c5171e0b4d2aa3bc1925775519852
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-10-24 13:04:17 -0700

    Fix return

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,4 +6,4 @@ class MyList(list):
     """ A class Mylist that inherits the mrthods of its Superclass 'list'"""
     def print_sorted(self):
         """ prints the list in a sorted ascending order"""
-        print(sorted(self)
+        print(sorted(self))
