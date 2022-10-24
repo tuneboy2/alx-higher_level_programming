aca16495a1aaab950dc6ca46de99c94033dd0028
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-10-24 16:44:33 -0700

    Task 5

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Defines a function inherits_from"""
+
+
+def inherits_from(obj, a_class):
+    """ Defines a function that checks if an object is an instance
+    of a class thats inherited from a specified class.
+
+    Args:
+        obj (class): Parameter to be checked
+        a_class (class): Parameter to be checked against
+
+    Return: 
+        True: if the object is an instance
+        False: if otherwise
+    """
+
+    return issubclass(obj, a_class)
---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Define a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """An empty instance of class BaseGeometry"""
+    pass
