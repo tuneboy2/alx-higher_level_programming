48697e2402b167f1ac1cda84c864065c05d3fac8
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-10-25 02:59:53 -0700

    Imported 7-base_geometry modile

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,28 +1,6 @@
 #!/usr/bin/python3
 """ Define a class BaseGeometry"""
-
-
-class BaseGeometry:
-    """An instance of class BaseGeometry"""
-    def area(self):
-        """ An undefinrd area method of class BaseGeometry"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ A method that validates a value
-
-        Args:
-            name (str): assume name is a string
-            value (int): parameter to be validated
-
-        Reaises:
-            TypeError: if value is not an integer
-            ValueError: if value is <= 0
-        """
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
