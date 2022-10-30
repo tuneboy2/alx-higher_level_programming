a1f7dbd571ddc96478370138997323e6786ee142
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-10-30 02:39:27 -0700

    Save arguments to a File

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,12 +9,12 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
-new = []
 try:
     old = load_from_json_file(filename)
-    for i in range(1, len(sys.argv)):
-        old.append(sys.argv[i])
-
-    save_to_json_file(old, filename)
 except Exception:
-    save_to_json_file(new, filename)
+    old = []
+
+for i in range(1, len(sys.argv)):
+    old.append(sys.argv[i])
+
+save_to_json_file(old, filename)
